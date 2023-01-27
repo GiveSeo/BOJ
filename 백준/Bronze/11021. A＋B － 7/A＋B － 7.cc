8c4540b86525008668ee925f9bc3443fa2a91577
To the Skies <76804466+To-the-Skies@users.noreply.github.com>
2023-01-27 23:33:43 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int test_case;
+    cin>>test_case;
+    for(int i=1;i<=test_case;i++){
+        int num1,num2;
+        cin>>num1>>num2;
+        cout<<"Case #"<<i<<": "<<num1+num2<<endl;         
+    }
+}
