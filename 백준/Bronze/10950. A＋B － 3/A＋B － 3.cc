e0e845662b42edfebbbdecec309310edfeab7821
To the Skies <76804466+To-the-Skies@users.noreply.github.com>
2023-01-27 23:09:39 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int test_case;
+    cin>>test_case;
+    for(int i=0;i<test_case;i++){
+        int num1,num2;
+        cin>>num1>>num2;
+        cout<<num1+num2<<endl;
+    }
+}
