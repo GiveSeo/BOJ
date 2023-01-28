5472e6fecda405010c4ec70721a776cc4c6c89c0
To the Skies <76804466+To-the-Skies@users.noreply.github.com>
2023-01-28 22:55:10 +0900

    Update 그대로 출력하기.cc

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -5,8 +5,8 @@ int main()
 {
     while(1){
         string temp;
-        getline(cin,temp);
-        if(temp=="")
+        getline(cin,temp);//공백을 포함하여 입력받음
+        if(temp=="")//입력받은게 공백이면 반복문 종료
             break;
         cout<<temp<<endl;
     }
