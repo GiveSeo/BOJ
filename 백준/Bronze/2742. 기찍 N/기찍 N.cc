4b363cabed7e3fc626da97d5e5c619ae7ec7f801
To the Skies <76804466+To-the-Skies@users.noreply.github.com>
2023-01-29 15:59:24 +0900

    Update 기찍 N.cc

--- 백준/Bronze/2742. 기찍 N/기찍 N.cc
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -7,5 +7,5 @@ int main()
     int num;
     cin>>num;
     for(int i=num;i>0;i--)
-        cout<<i<<"\n";
+        cout<<i<<"\n";//std::endl의 경우 버퍼를 비우는 과정이 있어서 "\n"을 쓰는 것보다 느리다.(실제로 std::endl를 쓰면 시간초과임.)
 }
