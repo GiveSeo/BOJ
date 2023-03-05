3d97860041647ebb19872beb9f5d9d2662918b8c
To the Skies <76804466+To-the-Skies@users.noreply.github.com>
2023-03-05 19:54:33 +0900

    Create 13023.cpp

---
+++ 백준/Gold/13023.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+vector<vector<int>> a;
+vector<bool> visited;
+bool ans=false;
+void DFS(int n, int dep);
+
+int main(){
+    int N,M;
+    cin>>N>>M;
+    a.resize(N+1);visited=vector<bool>(N+1,false);
+    for(int i=0;i<M;i++){
+        int s,v;
+        cin>>s>>v;
+        a[s].push_back(v);a[v].push_back(s);
+    }
+    for(int i=1;i<=N;i++){
+        DFS(i,1);
+        if(ans){break;}
+    }
+    if(ans){cout<<"1";}
+    else{cout<<"0";}
+}
+void DFS(int n, int dep){
+    if(dep==5||ans){
+        ans=true;
+        return;
+    }
+    visited[n]=true;
+    for(vector<int>::iterator iter=a[n].begin();iter!=a[n].end();iter++){
+        if(!visited[*iter]){
+            DFS(*iter,dep+1);
+            visited[*iter]=false;
+        }
+    }
+}
