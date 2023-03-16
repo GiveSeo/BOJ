64494fb298e6d0687d2503a8e56168eaf8de941e
To the Skies <76804466+To-the-Skies@users.noreply.github.com>
2023-03-16 12:06:19 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,11 @@
+// Example program
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    cout<<"\\    /\\"<<endl;
+    cout<<" )  ( ')"<<endl;
+    cout<<"(  \/  )"<<endl;
+    cout<<" \\(__)|"<<endl;
+}
