2eff50df21e7c397f100dfa628d3e304f6234aca
To the Skies <76804466+To-the-Skies@users.noreply.github.com>
2023-04-19 18:47:21 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,13 @@
+// Example program
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    cout<<".  .   ."<<endl;
+    cout<<"|  | _ | _. _ ._ _  _"<<endl;
+    cout<<"|/\\|(/.|(_.(_)[ | )(/."<<endl;
+    
+}
