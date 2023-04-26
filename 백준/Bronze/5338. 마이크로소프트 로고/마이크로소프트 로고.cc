f42a1f5fea4a7c6c9852a4f7d794cbad2df9afc6
To the Skies <76804466+To-the-Skies@users.noreply.github.com>
2023-04-26 22:06:04 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,12 @@
+// Example program
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    cout<<"       _.-;;-._"<<endl;
+    cout<<"'-..-'|   ||   |"<<endl;
+    cout<<"'-..-'|_.-;;-._|"<<endl;
+    cout<<"'-..-'|   ||   |"<<endl;
+    cout<<"'-..-'|_.-''-._|"<<endl;
+}
