b6d1bf5e472c4ba297b709d2a71587aab71cad61
To the Skies <76804466+To-the-Skies@users.noreply.github.com>
2023-04-27 19:54:02 +0900

    [Bronze V] Title: 아!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cc
@@ -0,0 +1,16 @@
+// Example program
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string temp1,temp2;
+    cin>>temp1>>temp2;
+    if(temp1.size()>=temp2.size()){
+        cout<<"go"<<endl;
+    }
+    else{
+        cout<<"no"<<endl;
+    }
+}
