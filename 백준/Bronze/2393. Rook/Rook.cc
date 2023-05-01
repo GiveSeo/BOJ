21d668130fe024a0539365f850b6dda98a4df7d2
To the Skies <76804466+To-the-Skies@users.noreply.github.com>
2023-05-01 22:08:21 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,15 @@
+// Example program
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+  cout<<"  ___  ___  ___"<<endl;
+  cout<<"  | |__| |__| |"<<endl;
+  cout<<"  |           |"<<endl;
+  cout<<"   \\_________/"<<endl;
+  cout<<"    \\_______/"<<endl;
+  cout<<"     |     |"<<endl;
+  cout<<"     |     |"<<endl;
+  cout<<"     |     |\n     |     |\n     |_____|\n  __/       \\__\n /             \\\n/_______________\\";
+}
