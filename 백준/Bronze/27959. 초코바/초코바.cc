6f108767db5d11347b11511a2cf2c48749902e61
To the Skies <76804466+To-the-Skies@users.noreply.github.com>
2023-05-11 22:16:25 +0900

    [Bronze V] Title: 초코바, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.cc
@@ -0,0 +1,15 @@
+// Example program
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    if(100*a>=b){
+        cout<<"Yes";
+    }
+    else{
+        cout<<"No";
+    }
+}
