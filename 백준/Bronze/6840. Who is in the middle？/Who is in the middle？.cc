d929feb032aad664c2f19cacbf21eba599685901
To the Skies <76804466+To-the-Skies@users.noreply.github.com>
2023-05-13 20:06:10 +0900

    [Bronze V] Title: Who is in the middle?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6840. Who is in the middle？/Who is in the middle？.cc
@@ -0,0 +1,10 @@
+#include<iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(){
+    vector<int> arr(3);
+    cin>>arr[0]>>arr[1]>>arr[2];
+    sort(arr.begin(),arr.end());
+    cout<<arr[1];
+}
