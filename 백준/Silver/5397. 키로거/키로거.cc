db40d7b59c1183add1115739841d7600100d7fcd
To the Skies <76804466+To-the-Skies@users.noreply.github.com>
2023-05-14 19:37:52 +0900

    [Silver II] Title: 키로거, Time: 460 ms, Memory: 19468 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <list>
+using namespace std;
+
+int n;
+int main(){
+    cin>>n;
+    for(int i=0;i<n;i++){
+        list<char> l;
+        string temp;
+        cin>>temp;
+        auto p = l.begin();
+        for(auto c : temp){
+            if(c == '<'){
+                if(p!=l.begin()){
+                    p--;
+                }
+            }
+            else if(c == '>'){
+                if(p!=l.end()){
+                    p++;
+                }
+            }
+            else if(c == '-'){
+                if(p!=l.begin()){
+                    p--;
+                    p=l.erase(p);
+                }
+            }
+            else if(c == '\n'){}
+            else{
+                l.insert(p,c);
+            }
+        }
+        for(auto c : l){
+           cout<<c;
+        }
+        cout<<"\n";
+    }
+}
