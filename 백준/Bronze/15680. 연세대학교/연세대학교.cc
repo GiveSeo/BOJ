4c9674ebf245ece4cc1169f92eef496ceb755f98
To the Skies <76804466+To-the-Skies@users.noreply.github.com>
2023-06-01 06:58:44 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,16 @@
+// Example program
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    if(n==0){
+        cout<<"YONSEI";
+    }
+    else{
+        cout<<"Leading the Way to the Future";
+    }
+}
